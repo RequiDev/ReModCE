0e291f1abe03a80c879583bab21a2488feeec116
Requi <privat@requi-dev.de>
2021-08-09 01:14:39 +0200

    Created solution with loader and mod projects.

---
+++ ReModCE.Loader/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(ReModCE.Loader.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(ReModCE.Loader.BuildInfo.Company)]
+[assembly: AssemblyProduct(ReModCE.Loader.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + ReModCE.Loader.BuildInfo.Author)]
+[assembly: AssemblyTrademark(ReModCE.Loader.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6e5e0e59-73d9-4d74-b734-d040d26710c4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(ReModCE.Loader.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(ReModCE.Loader.BuildInfo.Version)]
+
+[assembly: MelonInfo(typeof(ReModCE.Loader.ReLoader), ReModCE.Loader.BuildInfo.Name, ReModCE.Loader.BuildInfo.Version, ReModCE.Loader.BuildInfo.Author)]
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonColor(ConsoleColor.Magenta)]
---
+++ ReModCE.Loader/ReMod.Loader.cs
@@ -0,0 +1,70 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReModCE.Loader
+{
+    public static class BuildInfo
+    {
+        public const string Name = "ReModCE";
+        public const string Author = "Requi, FenrixTheFox";
+        public const string Company = null;
+        public const string Version = "1.0.0.0";
+    }
+
+    public class ReLoader : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+        }
+
+        public override void OnFixedUpdate()
+        {
+            base.OnFixedUpdate();
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+
+        public override void OnGUI()
+        {
+            base.OnGUI();
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasLoaded(buildIndex, sceneName);
+        }
+
+        public override void OnApplicationQuit()
+        {
+            base.OnApplicationQuit();
+        }
+
+        public override void OnLateUpdate()
+        {
+            base.OnLateUpdate();
+        }
+
+        public override void OnPreferencesLoaded()
+        {
+            base.OnPreferencesLoaded();
+        }
+
+        public override void OnPreferencesSaved()
+        {
+            base.OnPreferencesSaved();
+        }
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasInitialized(buildIndex, sceneName);
+        }
+    }
+}
