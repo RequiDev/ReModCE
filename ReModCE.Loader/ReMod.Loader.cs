19fa4efd24ca474f20d98a29e3d455e62c1a7158
Requi <privat@requi-dev.de>
2021-08-09 03:49:11 +0200

    Added functional basic loader.

--- ReModCE.Loader/ReMod.Loader.cs
+++ ReModCE.Loader/ReMod.Loader.cs
@@ -1,8 +1,11 @@
 ﻿using MelonLoader;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace ReModCE.Loader
@@ -17,54 +20,153 @@ namespace ReModCE.Loader
 
     public class ReLoader : MelonMod
     {
+        private Action _onApplicationStart;
+        private Action _onUiManagerInit;
+        private Action _onFixedUpdate;
+        private Action _onUpdate;
+        private Action _onGUI;
+        private Action _onApplicationQuit;
+        private Action _onLateUpdate;
+        private Action _onPreferencesLoaded;
+        private Action _onPreferencesSaved;
+
+        private Action<int, string> _onSceneWasLoaded;
+        private Action<int, string> _onSceneWasInitialized;
+
+
         public override void OnApplicationStart()
         {
-            base.OnApplicationStart();
+            var bytes = File.ReadAllBytes("ReModCE.dll");
+            Assembly assembly;
+            try
+            {
+                assembly = Assembly.Load(bytes);
+            }
+            catch (BadImageFormatException e)
+            {
+                MelonLogger.Error($"Couldn't load specified image: {e}");
+                return;
+            }
+
+            IEnumerable<Type> types;
+            try
+            {
+                types = assembly.GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                types = e.Types.Where(t => t != null);
+            }
+
+            var remodClass = types.FirstOrDefault(type => type.Name == "ReModCE");
+            if (remodClass == null)
+            {
+                MelonLogger.Error($"Couldn't find ReModCE class in assembly. ReModCE won't load.");
+                return;
+            }
+
+            var methods = remodClass.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (var m in methods)
+            {
+                var parameters = m.GetParameters();
+                switch (m.Name)
+                {
+                    case nameof(OnApplicationStart) when parameters.Length == 0:
+                        _onApplicationStart = (Action)Delegate.CreateDelegate(typeof(Action), m);
+                        break;
+                    case nameof(OnApplicationQuit) when parameters.Length == 0:
+                        _onApplicationQuit = (Action)Delegate.CreateDelegate(typeof(Action), m);
+                        break;
+                    case nameof(OnSceneWasLoaded) when parameters.Length == 2 && parameters[0].ParameterType == typeof(int) && parameters[1].ParameterType == typeof(string):
+                        _onSceneWasLoaded = (Action<int, string>)Delegate.CreateDelegate(typeof(Action<int, string>), m);
+                        break;
+                    case nameof(OnSceneWasInitialized) when parameters.Length == 2 && parameters[0].ParameterType == typeof(int) && parameters[1].ParameterType == typeof(string):
+                        _onSceneWasInitialized = (Action<int, string>)Delegate.CreateDelegate(typeof(Action<int, string>), m);
+                        break;
+                    case nameof(OnUpdate) when parameters.Length == 0:
+                        _onUpdate = (Action)Delegate.CreateDelegate(typeof(Action), m);
+                        break;
+                    case nameof(OnUiManagerInit) when parameters.Length == 0:
+                        _onUiManagerInit = (Action)Delegate.CreateDelegate(typeof(Action), m);
+                        break;
+                    case nameof(OnGUI) when parameters.Length == 0:
+                        _onGUI = (Action)Delegate.CreateDelegate(typeof(Action), m);
+                        break;
+                    case nameof(OnLateUpdate) when parameters.Length == 0:
+                        _onLateUpdate = (Action)Delegate.CreateDelegate(typeof(Action), m);
+                        break;
+                    case nameof(OnFixedUpdate) when parameters.Length == 0:
+                        _onFixedUpdate = (Action)Delegate.CreateDelegate(typeof(Action), m);
+                        break;
+                    case nameof(OnPreferencesLoaded) when parameters.Length == 0:
+                        _onPreferencesLoaded = (Action)Delegate.CreateDelegate(typeof(Action), m);
+                        break;
+                    case nameof(OnPreferencesSaved) when parameters.Length == 0:
+                        _onPreferencesSaved = (Action)Delegate.CreateDelegate(typeof(Action), m);
+                        break;
+                }
+            }
+
+            MelonCoroutines.Start(WaitForUiManager());
+            _onApplicationStart();
+        }
+
+        public void OnUiManagerInit()
+        {
+            _onUiManagerInit();
         }
 
         public override void OnFixedUpdate()
         {
-            base.OnFixedUpdate();
+            _onFixedUpdate();
         }
 
         public override void OnUpdate()
         {
-            base.OnUpdate();
-        }
-
-        public override void OnGUI()
-        {
-            base.OnGUI();
-        }
-
-        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
-        {
-            base.OnSceneWasLoaded(buildIndex, sceneName);
-        }
-
-        public override void OnApplicationQuit()
-        {
-            base.OnApplicationQuit();
+            _onUpdate();
         }
 
         public override void OnLateUpdate()
         {
-            base.OnLateUpdate();
+            _onLateUpdate();
         }
 
-        public override void OnPreferencesLoaded()
+        public override void OnGUI()
         {
-            base.OnPreferencesLoaded();
+            _onGUI();
         }
 
-        public override void OnPreferencesSaved()
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            base.OnPreferencesSaved();
+            _onSceneWasLoaded(buildIndex, sceneName);
         }
 
         public override void OnSceneWasInitialized(int buildIndex, string sceneName)
         {
-            base.OnSceneWasInitialized(buildIndex, sceneName);
+            _onSceneWasInitialized(buildIndex, sceneName);
+        }
+
+        public override void OnApplicationQuit()
+        {
+            _onApplicationQuit();
+        }
+
+        public override void OnPreferencesLoaded()
+        {
+            _onPreferencesLoaded();
+        }
+
+        public override void OnPreferencesSaved()
+        {
+            _onPreferencesSaved();
+        }
+
+        private IEnumerator WaitForUiManager()
+        {
+            while (VRCUiManager.field_Private_Static_VRCUiManager_0 == null) yield return null;
+            while (QuickMenu.prop_QuickMenu_0 == null) yield return null;
+
+            OnUiManagerInit();
         }
     }
 }
--- ReModCE/ReMod.cs
+++ ReModCE/ReMod.cs
@@ -4,10 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MelonLoader;
+using ReModCE.Loader;
 
 namespace ReModCE
 {
-    public class ReMod : MelonMod
+    public static class ReModCE
     {
+        public static void OnApplicationStart()
+        {
+            ReLogger.Msg(nameof(OnApplicationStart));
+        }
+        public static void OnUiManagerInit()
+        {
+            ReLogger.Msg(nameof(OnUiManagerInit));
+        }
+
+        public static void OnFixedUpdate()
+        {
+            ReLogger.Msg(nameof(OnFixedUpdate));
+        }
+
+        public static void OnUpdate()
+        {
+            ReLogger.Msg(nameof(OnUpdate));
+        }
+
+        public static void OnLateUpdate()
+        {
+            ReLogger.Msg(nameof(OnLateUpdate));
+        }
+
+        public static void OnGUI()
+        {
+            ReLogger.Msg(nameof(OnGUI));
+        }
+
+        public static void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            ReLogger.Msg(nameof(OnSceneWasLoaded));
+        }
+
+        public static void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            ReLogger.Msg(nameof(OnSceneWasInitialized));
+        }
+
+        public static void OnApplicationQuit()
+        {
+            ReLogger.Msg(nameof(OnApplicationQuit));
+        }
+
+        public static void OnPreferencesLoaded()
+        {
+            ReLogger.Msg(nameof(OnPreferencesLoaded));
+        }
+
+        public static void OnPreferencesSaved()
+        {
+            ReLogger.Msg(nameof(OnPreferencesSaved));
+        }
     }
 }
