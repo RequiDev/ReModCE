bb2f6b7314f7ea7c92b9da154bf77c5db7d056e2
Requi <privat@requi-dev.de>
2021-08-09 08:58:21 +0200

    Only call MelonPreferences.CreateCategory if necessary.

--- ReModCE/Core/ConfigValue.cs
+++ ReModCE/Core/ConfigValue.cs
@@ -10,11 +10,12 @@ namespace ReModCE.Core
     internal class ConfigValue<T>
     {
         private readonly MelonPreferences_Entry<T> _entry;
+        private static MelonPreferences_Category _category;
 
         public ConfigValue(string name, T defaultValue, string displayName = null, string description = null, bool isHidden = false)
         {
-            var category = MelonPreferences.CreateCategory("ReModCE");
-            _entry = category.GetEntry<T>(name) ?? category.CreateEntry(name, defaultValue, displayName, description, isHidden);
+            _category ??= MelonPreferences.CreateCategory("ReModCE");
+            _entry = _category.GetEntry<T>(name) ?? _category.CreateEntry(name, defaultValue, displayName, description, isHidden);
         }
 
         public static implicit operator T(ConfigValue<T> conf)
