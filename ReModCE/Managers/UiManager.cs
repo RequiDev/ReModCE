82263a1032863d785af8cbd9b96d51fc1f7b7fbf
Requi <privat@requi-dev.de>
2021-08-13 10:36:04 +0200

    Small refactor in UiManager.

--- ReModCE/Managers/UiManager.cs
+++ ReModCE/Managers/UiManager.cs
@@ -11,11 +11,9 @@ namespace ReModCE.Managers
     internal class UiManager
     {
         public const float ButtonSize = 420f;
-        private readonly ReQuickMenu _mainMenu;
-        private readonly ReQuickMenu _targetMenu;
 
-        public ReQuickMenu MainMenu => _mainMenu;
-        public ReQuickMenu TargetMenu => _targetMenu;
+        public ReQuickMenu MainMenu { get; }
+        public ReQuickMenu TargetMenu { get; }
 
         public bool IsRemodLoaded { get; }
 
@@ -31,18 +29,18 @@ namespace ReModCE.Managers
 
             var intialButtonPos = ExtendedQuickMenu.ReportWorldButton.GetComponent<RectTransform>().localPosition;
             
-            _mainMenu = new ReQuickMenu(menuName);
+            MainMenu = new ReQuickMenu(menuName);
             ReQuickButton.Create(new Vector2(intialButtonPos.x + (Convert.ToInt32(IsRemodLoaded) * ButtonSize), intialButtonPos.y + (ButtonSize * 2f)),
                 "ReMod <color=#00ff00>CE</color>", "Access the ReMod Community Edition",
-                () => _mainMenu.Open(),
+                () => MainMenu.Open(),
                 ExtendedQuickMenu.ShortcutMenu);
 
-            _mainMenu.AddSubMenu("Movement", "Access movement related options");
+            MainMenu.AddSubMenu("Movement", "Access movement related options");
 
-            _targetMenu = new ReQuickMenu("TargetReModCE", "UserInteractMenu", QuickMenuContext.UserSelected);
+            TargetMenu = new ReQuickMenu("TargetReModCE", "UserInteractMenu", QuickMenuContext.UserSelected);
             ReQuickButton.Create(new Vector2(intialButtonPos.x + (Convert.ToInt32(IsRemodLoaded) * ButtonSize), intialButtonPos.y - (ButtonSize * 2f)),
                 "ReMod <color=#00ff00>CE</color> Target Options", "More options for this target",
-                () => _targetMenu.Open(QuickMenuContext.UserSelected),
+                () => TargetMenu.Open(QuickMenuContext.UserSelected),
                 ExtendedQuickMenu.UserInteractMenu.transform);
         }
     }
