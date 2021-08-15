e2fb5fb1c66145ead621ad08b63ce8a289de1e7f
Requi <privat@requi-dev.de>
2021-08-15 20:49:11 +0200

    Added ability to move main menu button to different location using UIX/MelonPreferences.

--- ReModCE/Managers/UiManager.cs
+++ ReModCE/Managers/UiManager.cs
@@ -2,9 +2,12 @@
 using System.IO;
 using System.Linq;
 using MelonLoader;
+using ReModCE.Core;
+using ReModCE.Loader;
 using ReModCE.UI;
 using ReModCE.VRChat;
 using UnityEngine;
+using UnityEngine.UI;
 using QuickMenuContext = QuickMenuContextualDisplay.EnumNPublicSealedvaUnNoToUs7vUsNoUnique;
 
 namespace ReModCE.Managers
@@ -12,28 +15,45 @@ namespace ReModCE.Managers
     internal class UiManager
     {
         public const float ButtonSize = 420f;
+        private Vector3 _intialButtonPos;
 
+        private ReQuickButton _mainMenuButton;
         public ReQuickMenu MainMenu { get; }
         public ReQuickMenu TargetMenu { get; }
 
         public bool IsRemodLoaded { get; }
         public bool IsRubyLoaded { get; }
 
+        private ConfigValue<float> ButtonOffsetX;
+        private ConfigValue<float> ButtonOffsetY;
         public UiManager(string menuName)
         {
+            ButtonOffsetX = new ConfigValue<float>(nameof(ButtonOffsetX), 0f, "Main Button Offset X", "Offset on the X axis for the main menu button. Relative to \"Report World\" button. 1 = 1 button size.");
+            ButtonOffsetX.OnValueChanged += OnButtonOffsetChanged;
+            ButtonOffsetY = new ConfigValue<float>(nameof(ButtonOffsetY), 2f, "Main Button Offset X", "Offset on the Y axis for the main menu button. Relative to \"Report World\" button. 1 = 1 button size.");
+            ButtonOffsetY.OnValueChanged += OnButtonOffsetChanged;
+
+            var buttonOffset = new Vector3(ButtonSize * ButtonOffsetX, ButtonSize * ButtonOffsetY);
+
             IsRemodLoaded = MelonHandler.Mods.Any(m => m.Info.Name == "ReMod");
             IsRubyLoaded = File.Exists("hid.dll");
 
-            if (!IsRemodLoaded)
+            var isDefaultButtonPos = buttonOffset == new Vector3(0, ButtonSize * 2f);
+            if (IsRemodLoaded && isDefaultButtonPos)
+            {
+                buttonOffset.x = ButtonSize;
+            }
+
+            if (!IsRemodLoaded && isDefaultButtonPos)
             {
                 ExtendedQuickMenu.ShortcutMenu.Find("UserIconCameraButton").localPosition +=
                     new Vector3(ButtonSize, -ButtonSize, 0f);
             }
 
-            var intialButtonPos = ExtendedQuickMenu.ReportWorldButton.GetComponent<RectTransform>().localPosition;
+            _intialButtonPos = ExtendedQuickMenu.ReportWorldButton.GetComponent<RectTransform>().localPosition;
             
             MainMenu = new ReQuickMenu(menuName);
-            ReQuickButton.Create(new Vector2(intialButtonPos.x + (Convert.ToInt32(IsRemodLoaded) * ButtonSize), intialButtonPos.y + (ButtonSize * 2f)),
+            _mainMenuButton = new ReQuickButton(_intialButtonPos + buttonOffset,
                 "ReMod <color=#00ff00>CE</color>", "Access the ReMod Community Edition",
                 () => MainMenu.Open(),
                 ExtendedQuickMenu.ShortcutMenu);
@@ -41,10 +61,16 @@ namespace ReModCE.Managers
             MainMenu.AddSubMenu("Movement", "Access movement related options");
 
             TargetMenu = new ReQuickMenu("TargetReModCE", "UserInteractMenu", QuickMenuContext.UserSelected);
-            ReQuickButton.Create(new Vector2(intialButtonPos.x + (Convert.ToInt32(IsRemodLoaded) * ButtonSize), intialButtonPos.y - (ButtonSize * 2f)),
+            ReQuickButton.Create(new Vector2(_intialButtonPos.x + (Convert.ToInt32(IsRemodLoaded) * ButtonSize), _intialButtonPos.y - (ButtonSize * 2f)),
                 "ReMod <color=#00ff00>CE</color> Target Options", "More options for this target",
                 () => TargetMenu.Open(QuickMenuContext.UserSelected),
                 ExtendedQuickMenu.UserInteractMenu.transform);
+        }
+
+        private void OnButtonOffsetChanged()
+        {
+            var buttonOffset = new Vector3(ButtonSize * ButtonOffsetX, ButtonSize * ButtonOffsetY);
+            _mainMenuButton.Position = _intialButtonPos + buttonOffset;
         }
     }
 }
