003fd1556b6014c0f2dc69a366378a6ddb7fb8ba
Requi <privat@requi-dev.de>
2022-06-11 22:37:24 +0200

    Always allow risky functions in Friends, Invite+ and Invite only worlds.

--- ReModCE/Managers/RiskyFunctionsManager.cs
+++ ReModCE/Managers/RiskyFunctionsManager.cs
@@ -6,6 +6,7 @@ using MelonLoader;
 using ReMod.Core;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using VRC.Core;
 
 namespace ReModCE.Managers
 {
@@ -42,24 +43,35 @@ namespace ReModCE.Managers
         private IEnumerator CheckWorld()
         {
             while (RoomManager.field_Internal_Static_ApiWorld_0 == null) yield return new WaitForEndOfFrame();
+            
             var apiWorld = RoomManager.field_Internal_Static_ApiWorld_0;
+            var apiWorldInstance = RoomManager.field_Internal_Static_ApiWorldInstance_0;
 
-            var worldName = apiWorld.name.ToLower();
-            var tags = new List<string>();
-            foreach (var tag in apiWorld.tags)
+            bool riskyFunctionAllowed;
+            if (apiWorldInstance.type <= InstanceAccessType.FriendsOfGuests)
             {
-                tags.Add(tag.ToLower());
-            }
+                var worldName = apiWorld.name.ToLower();
+                var tags = new List<string>();
+                foreach (var tag in apiWorld.tags)
+                {
+                    tags.Add(tag.ToLower());
+                }
 
-            var hasBlacklistedTag = _blacklistedTags.Any(tag => tags.Contains(tag));
-            var riskyFunctionAllowed = !worldName.Contains("club") && !worldName.Contains("game") && !hasBlacklistedTag;
+                var hasBlacklistedTag = _blacklistedTags.Any(tag => tags.Contains(tag));
+                riskyFunctionAllowed =
+                    !worldName.Contains("club") && !worldName.Contains("game") && !hasBlacklistedTag;
 
-            var rootGameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
-            if (rootGameObjects.Any(go => go.name is "eVRCRiskFuncDisable" or "UniversalRiskyFuncDisable"))
-            {
-                riskyFunctionAllowed = false;
+                var rootGameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+                if (rootGameObjects.Any(go => go.name is "eVRCRiskFuncDisable" or "UniversalRiskyFuncDisable"))
+                {
+                    riskyFunctionAllowed = false;
+                }
+                else if (rootGameObjects.Any(go => go.name is "eVRCRiskFuncEnable" or "UniversalRiskyFuncEnable"))
+                {
+                    riskyFunctionAllowed = true;
+                }
             }
-            else if (rootGameObjects.Any(go => go.name is "eVRCRiskFuncEnable" or "UniversalRiskyFuncEnable"))
+            else
             {
                 riskyFunctionAllowed = true;
             }
